3bcc5c82e1152817ea99402e131035b193434437
yan <1481103146>
2023-08-03 20:04:17 +0800

    省

---
+++ CoolWeather/app/src/main/java/com/lzok/coolweather/db/Province.java
@@ -0,0 +1,43 @@
+package com.lzok.coolweather.db;
+
+import android.text.method.QwertyKeyListener;
+
+import com.qweather.sdk.view.QWeather;
+
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * @author lzok
+ * 省
+ */
+public class Province extends LitePalSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
