b57fa3294b44189518e48d9dcff5fb93d5ab1850
yan <1481103146>
2023-09-03 16:16:20 +0800

    添加搜索activity

---
+++ RssRead/app/src/main/java/com/lzok/rssread/AddConnection.java
@@ -0,0 +1,14 @@
+package com.lzok.rssread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddConnection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_connection);
+    }
+}
