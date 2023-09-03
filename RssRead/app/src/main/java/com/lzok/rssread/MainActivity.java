a41bc831c7a8e9be56556c34d5045fe569da00c5
yan <1481103146>
2023-09-03 16:18:01 +0800

    添加悬浮按扭事件监听

--- RssRead/app/src/main/java/com/lzok/rssread/MainActivity.java
+++ RssRead/app/src/main/java/com/lzok/rssread/MainActivity.java
@@ -1,17 +1,34 @@
 package com.lzok.rssread;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * @author lzok
  */
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    FloatingActionButton floating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        floating = findViewById(R.id.fab);
+
+        floating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AddConnection.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
