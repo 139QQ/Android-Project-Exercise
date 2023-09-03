7f5a755e17fd7becafc1544d088e41c47b3ef4b2
yan <1481103146>
2023-09-03 14:42:50 +0800

    创建Rss阅读器

---
+++ RssRead/app/src/main/java/com/lzok/rssread/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lzok.rssread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author lzok
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
