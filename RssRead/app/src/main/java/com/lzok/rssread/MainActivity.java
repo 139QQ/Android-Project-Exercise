f7fc48b72b18a7195d4303b3e90db7145d407e32
yan <1481103146>
2023-09-16 16:22:48 +0800

    实现加载图片等功能

--- RssRead/app/src/main/java/com/lzok/rssread/MainActivity.java
+++ RssRead/app/src/main/java/com/lzok/rssread/MainActivity.java
@@ -2,34 +2,32 @@ package com.lzok.rssread;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.widget.NestedScrollView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.MaterialToolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.NestedScrollView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.lzok.rssread.Data.DataPersistenceManager;
 import com.lzok.rssread.Data.RssFeed;
 import com.lzok.rssread.Util.HtmlDownloader;
 
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -39,11 +37,13 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     FloatingActionButton floating;
     RssFeed rssFeed = new RssFeed();
-   List<RssFeed> rssFeeds = new ArrayList<>();
-
-    TextView text_hint;
-    AppBarLayout app_bar;
+    DrawerLayout drawerLayout ;
+    List<RssFeed> rssItems = rssFeed.getItems();
+    private DataPersistenceManager dataPersistenceManager = null;
+    TextView text_hint,like;
     NestedScrollView nestedScrollView ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +53,9 @@ public class MainActivity extends AppCompatActivity {
         floating = findViewById(R.id.fab);
         text_hint = findViewById(R.id.text_hint);
         nestedScrollView = findViewById(R.id.nested_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
 
-
+        dataPersistenceManager = DataPersistenceManager.getInstance(this);
 
         floating.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         new DownloadRssFeedTask().execute();
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
 
 
 
@@ -88,26 +95,43 @@ public class MainActivity extends AppCompatActivity {
 
 
     private class DownloadRssFeedTask extends AsyncTask<Void, Void, RssFeed> {
+        Intent intent = getIntent();
         @Override
         protected RssFeed doInBackground(Void... params) {
-            Intent intent = getIntent();
-
             HtmlDownloader htmlDownloader = new HtmlDownloader();
-            String url = intent.getStringExtra("url");
-            if (url == null){
+            String url = null;
+
+            // 尝试从DataPersistenceManager获取最后一次打开的连接
+            String lastOpenedLink = dataPersistenceManager.getLastOpenedLink();
+            if (!lastOpenedLink.isEmpty()) {
+                url = lastOpenedLink;
+                Log.i(TAG, "Last opened link: " + url);
+            } else {
+                // 如果没有最后一次打开的连接，尝试从Intent获取
+                Intent intent = getIntent();
+                url = intent.getStringExtra("url");
+                // 如果从Intent中获取到链接，将其保存为最后一次打开的连接
+                if (url != null && !url.isEmpty()) {
+                    dataPersistenceManager.saveLastOpenedLink(url);
+                }
+            }
+
+            if (url == null || url.isEmpty()) {
                 return new RssFeed();
             }
+
             text_hint.setVisibility(View.GONE);
-//            app_bar.setVisibility(View .VISIBLE);
             nestedScrollView.setVisibility(View.VISIBLE);
-            Log.i(TAG, "doInBackground: "+ url);
+            Log.i(TAG, "doInBackground: " + url);
 
             return htmlDownloader.downloadRssFeed(url);
+
         }
 
         @Override
         protected void onPostExecute(RssFeed rssFeed) {
             if (rssFeed != null) {
+
                 toolbar.setTitle(rssFeed.getChannel());
                 Log.i(TAG, "onCreate: " + rssFeed.getChannel());
                 // 更新RecyclerView或其他UI组件
