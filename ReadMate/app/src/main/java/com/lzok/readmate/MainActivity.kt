904ec5ef544237b5493e194e05db99bae5c7320d
yan <1481103146>
2023-12-09 17:36:00 +0800

    添加导航

--- ReadMate/app/src/main/java/com/lzok/readmate/MainActivity.kt
+++ ReadMate/app/src/main/java/com/lzok/readmate/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -19,21 +14,40 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.lzok.readmate.network.RssHubparse
 import com.lzok.readmate.ui.theme.ReadMateTheme
 import com.lzok.readmate.item.NewsListItem
 import com.lzok.readmate.item.NewsListItemContent
-import java.nio.file.WatchEvent
+import com.lzok.readmate.view.news.Read
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ReadMateTheme {
-                MyScreen()
+                JumpRead()
 
+            }
+        }
+    }
+
+    /**
+     * 跳转到Read页面
+     */
+    @Composable
+    private fun JumpRead() {
+        val navController = rememberNavController() // 初始化 NavController 对象
+        NavHost(navController = navController, startDestination = "main") {
+            // 定义应用程序的导航图
+            composable("main") {
+                MyScreen(navController)
+            }
+            composable("read") {
+                Read()
             }
         }
     }
@@ -44,7 +58,7 @@ class MainActivity : ComponentActivity() {
  *
  */
 @Composable
-fun MyScreen() {
+fun MyScreen(navController: NavController) {
     val rssHubparse = RssHubparse()
     val rssUrl = "https://rsshub.rssforever.com/36kr/information/web_news" // 替换为你的 RSS 订阅源链接
     var newsItems by remember { mutableStateOf<List<NewsListItem>>(emptyList()) }
@@ -54,23 +68,30 @@ fun MyScreen() {
         newsItems = items
     }
 
-    MainScreen(newsItems)
+    MainScreen(newsItems, navController)
 }
+
 /*用于显示新闻列表*/
 @Composable
-fun NewsList(newsList: List<NewsListItem>) {
+fun NewsList(newsList: List<NewsListItem>, navController: NavController) {
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(newsList) { news ->
-            NewsListItemContent(news)
+            NewsListItemContent(news) {
+                navController.navigate("read")
+
+            }
         }
     }
 }
+
+
 /*用于显示主界面的内容*/
 @Composable
-fun MainScreen(newsList: List<NewsListItem>) {
+fun MainScreen(newsList: List<NewsListItem>, navController: NavController) {
+
     Column {
         // 其他主界面组件
-        NewsList(newsList)
+        NewsList(newsList, navController)
     }
 }
 
